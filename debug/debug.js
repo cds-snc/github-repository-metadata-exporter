6a9f77491ccdf1ee9ce881fe4f814fe9b9183117
Alvaro Scheid <ascheid@gmail.com>
2022-12-07 15:03:57 -0500

    fix: pointing to a specific repo

--- debug/debug.js
+++ debug/debug.js
@@ -63,7 +63,7 @@ const run = async () => {
     console.log(codeScanningAlertsData);
 
     // Get Renovate PRs data
-    const renovatePRs = await queryRenovatePRs(octokit, owner, repo);
+    const renovatePRs = await queryRenovatePRs(octokit, 'cds-snc', 'gcds-components');
     console.log("Renovate PRs data");
     console.log("======================");
     console.log(renovatePRs);
