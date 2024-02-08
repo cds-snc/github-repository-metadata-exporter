41164d7ecae383d8703e195d43f67707336a19f4
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2024-02-08 22:28:25 +0000

    Adding prettier

--- src/lib/query.js
+++ src/lib/query.js
@@ -214,15 +214,20 @@ const queryDependabotAlerts = async (octokit, owner, repo) => {
           }
         }
       });
-    } catch (error) {
-      if (error.status === 403 && error.response && error.response.data.message === 'Dependabot alerts are disabled for this repository.') {
-         // Optionally handle this case specifically, e.g., by setting alerts to a specific value or returning a custom error message
-        console.log("❌ Dependabot alerts are disabled for this repository.");
-      } else {
-        // Handle other errors or a 403 without the specific message
-        console.error("An error occurred:", error);
-      }
+  } catch (error) {
+    if (
+      error.status === 403 &&
+      error.response &&
+      error.response.data.message ===
+        "Dependabot alerts are disabled for this repository."
+    ) {
+      // Optionally handle this case specifically, e.g., by setting alerts to a specific value or returning a custom error message
+      console.log("❌ Dependabot alerts are disabled for this repository.");
+    } else {
+      // Handle other errors or a 403 without the specific message
+      console.error("An error occurred:", error);
     }
+  }
 
   return {
     metadata_owner: owner,
